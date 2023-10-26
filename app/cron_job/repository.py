2e41ab1dc5eb37035cbe6e92053c3ba45eef4ace
honeydreamchaser <honeydreamchaser@outlook.com>
2023-10-26 04:12:01 +0200

    search_box add

--- app/cron_job/repository.py
+++ app/cron_job/repository.py
@@ -25,11 +25,12 @@ class CronHistoryRepo:
             cur_month = datetime.datetime.today().month
             cur_year = datetime.datetime.today().year
             created_at = datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S")
-            db.query(CronHistory).\
+            select = db.query(CronHistory).\
                 filter(and_(CronHistory.user_id == user_id,
                             extract("month", CronHistory.created_at) == cur_month,
-                            extract("year", CronHistory.created_at) == cur_year)).\
-                update({CronHistory.total_search_result: cronhistory_data["total_search_result"],
+                            extract("year", CronHistory.created_at) == cur_year))
+            print(select.first())
+            select.update({CronHistory.total_search_result: cronhistory_data["total_search_result"],
                         CronHistory.positive_search_result: cronhistory_data["positive_search_result"],
                         CronHistory.negative_search_result: cronhistory_data["negative_search_result"],
                         CronHistory.created_at: created_at})
