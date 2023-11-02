5d331a78fefd9cc9d96be280174ad9f0a02e0f0b
honeydreamchaser <honeydreamchaser@outlook.com>
2023-11-02 15:53:54 +0100

    quote send bug fix

--- app/intervention/repository.py
+++ app/intervention/repository.py
@@ -470,7 +470,6 @@ class InterventionRepo:
             inter_response = db.query(InterventionResponse).\
                                 filter(and_(InterventionResponse.request_id == intervention_id,
                                             InterventionResponse.response_type == 1)).first()
-            print(inter_response)
             if inter_response == None:
                 return False
             return True
