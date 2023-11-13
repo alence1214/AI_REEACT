867a1ca2f9a29506d7851e3220f84953fe9190fd
honeydreamchaser <honeydreamchaser@outlook.com>
2023-11-13 02:32:32 +0100

    deploying

--- main.py
+++ main.py
@@ -33,5 +33,5 @@ cronjobModel.Base.metadata.create_all(bind=engine)
 emailverifyModel.Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=443, log_level="debug", ssl_certfile="certificate.crt", ssl_keyfile="private.key", workers=2, reload=False)
-    # uvicorn.run(main_api.app, host="0.0.0.0", port=8000, log_level="debug", workers=2, worker_class="uvicorn.workers.UvicornWorker")
+    # uvicorn.run("app.api:app", host="0.0.0.0", port=443, log_level="debug", ssl_certfile="certificate.crt", ssl_keyfile="private.key", workers=2, reload=False)
+    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, log_level="debug", workers=2, worker_class="uvicorn.workers.UvicornWorker")
