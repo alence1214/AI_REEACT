9502309563d7246832f6ec56c9bb4388e6d600dd
honeydreamchaser <138431748+honeydreamchaser@users.noreply.github.com>
2023-12-15 16:58:26 +0100

    2fa remove on app.reeact.io

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://honey:honey@127.0.0.1:3306/reeact"
-# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://honey:honey@127.0.0.1:3306/reeact"
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:password@127.0.0.1:3306/reeact_test"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://honey:honey@127.0.0.1:3306/reeact"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:password@127.0.0.1:3306/reeact_test"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
